3ede70723e23ea1e621e188672e1d3e3152d123c
AlexisLadam <alexis.ladam@epitech.eu>
2025-10-17 12:46:01 +0200

    feat(6-configuration-title-et-favicon): change brandName for app name

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,9 +28,11 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('home')
             ->login()
+            ->brandName(fn () => __('filament.app.name'))
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->brandLogo(null)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
