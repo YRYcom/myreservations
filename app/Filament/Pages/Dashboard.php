a0242e52659fe16ee8ba63b63540aa0f96a90f5e
AlexisLadam <alexis.ladam@epitech.eu>
2025-10-17 11:42:43 +0200

    fix(5-nettoyage-de-linterface): change traduction method for __() from lang:get

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -3,24 +3,23 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Dashboard as BaseDashboard;
-use Illuminate\Support\Facades\Lang;
 
 class Dashboard extends BaseDashboard
 {
     public function getTitle(): string
     {
-        return Lang::get('filament.dashboard');
+        return __('filament.dashboard');
     }
 
     public function getHeading(): string
     {
-        return Lang::get('filament.dashboard');
+        return __('filament.dashboard');
     }
 
     protected static ?string $navigationLabel = null;
 
     public static function getNavigationLabel(): string
     {
-        return Lang::get('filament.dashboard');
+        return __('filament.dashboard');
     }
 }
