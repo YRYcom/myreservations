ae3d3bf8b8f3ce02aff91e711d7b9920be273e25
AlexisLadam <alexis.ladam@epitech.eu>
2025-10-31 09:19:29 +0100

    feat(8_gestion_des_biens): add french traduction

--- resources/lang/fr/filament.php
+++ resources/lang/fr/filament.php
@@ -3,4 +3,12 @@
 return [
     'dashboard' => 'Accueil',
     'app.name' => 'Mes Réservations',
+    'resources.biens.name' => 'Nom',
+    'resources.biens.created_at' => 'Créé le',
+    'resources.biens.updated_at' => 'Mis à jour le',
+    'resources.biens.delete' => 'Supprimer',
+    'resources.biens.edit' => 'Modifier le bien',
+    'resources.biens.create' => 'Créer un bien',
+    'resources.biens.index' => 'Liste des biens',
+    'resources.biens.show' => 'Voir le bien',
 ];
