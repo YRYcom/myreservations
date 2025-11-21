29224cf42830ebe247e55ee21d9f922e36dadbcf
AlexisLadam <alexis.ladam@epitech.eu>
2025-11-21 14:14:29 +0100

    feat(33_masquer_les_reservations_termines): add translation

--- resources/lang/en/filament.php
+++ resources/lang/en/filament.php
@@ -45,4 +45,5 @@ return [
     'resources.reservations.comment' => 'Comment',
     'resources.reservations.occupant' => 'Occupant',
     'resources.reservations.occupant.name.unique' => 'The occupant name must be unique.',
+    'resources.reservations.filters.hide_finished' => 'Hide finished reservations',
 ];
