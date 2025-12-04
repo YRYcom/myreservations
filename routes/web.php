afc55fa542c2c41da2e542945fb2dde1e3e817c3
AlexisLadam <alexis.ladam@epitech.eu>
2025-12-04 15:48:20 +0100

    feat(33_masquer_les_reservations_termines): add displayFinishedToggle

--- routes/web.php
+++ routes/web.php
@@ -9,30 +9,7 @@ Route::get('/', function () {
 
 Route::middleware('auth')->post('/reservations/display-finished', function (Request $request) {
     session(['display_finished' => $request->boolean('display_finished')]);
-
-    // Valider que l'URL de redirection est locale et sécurisée
-    $redirectTo = $request->input('redirect_to');
-    $validatedUrl = null;
-    
-    if ($redirectTo) {
-        // Vérifier que l'URL commence par l'URL de base de l'application
-        $appUrl = config('app.url');
-        $parsedRedirect = parse_url($redirectTo);
-        $parsedApp = parse_url($appUrl);
-        
-        // Autoriser seulement les URLs du même domaine
-        if (isset($parsedRedirect['host']) && isset($parsedApp['host'])) {
-            if ($parsedRedirect['host'] === $parsedApp['host']) {
-                $validatedUrl = $redirectTo;
-            }
-        } elseif (!isset($parsedRedirect['host'])) {
-            // URL relative, c'est sûr
-            $validatedUrl = $redirectTo;
-        }
-    }
-
     return response()->json([
         'success' => true,
-        'redirect_to' => $validatedUrl,
     ]);
 })->name('reservations.display-finished');
